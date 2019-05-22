2c4eba509ab71abb0b7ec62fc2da3402d2d34a7d
Zorgik <richegor97@gmail.com>
2019-05-22 10:57:50 +0300

    Fix mysql

---
+++ app/Flight.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Flight extends Model
+{
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'flight_id';
+}
